bba209ad2908600e6b4b59c33c25ae14ecf15049
email4prasanth <email4prasanth@gmail.com>
2025-04-16 16:00:32 +0530

    added pre-processing module

--- src/data_collection.py
+++ src/data_collection.py
@@ -3,11 +3,11 @@ import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("D:\PersonalRepo\MLOPS\Data_Collection\water_potability.csv")
+data = pd.read_csv(r"D:\PersonalRepo\MLOPS\Lecture_Notes\Data_Collection\water_potability.csv")
 
 train_data, test_data = train_test_split(data, test_size= 0.2, random_state = 40)
 
 data_path = os.path.join("data", "raw")
 os.makedirs(data_path)
-train_data.to_csv(os.path.join(data_path, "train_csv"), index=False)
-test_data.to_csv(os.path.join(data_path, "test_data"), index= False)
+train_data.to_csv(os.path.join(data_path, "train.csv"), index=False)
+test_data.to_csv(os.path.join(data_path, "test.csv"), index= False)
---
+++ src/data_preprocessing.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+
+# input
+train_data =  pd.read_csv("./data/raw/train.csv")
+test_data =  pd.read_csv("./data/raw/test.csv")
+
+# Median fuction to fill the missing values 
+def missing_vlaues_meadian(df):
+    for column in df.columns:
+        if df[column].isnull().any():
+            median_value = df[column].median()
+            df[column].fillna(median_value, inplace=True)
+    return df
+train_processed_data = missing_vlaues_meadian(train_data)
+test_processed_data = missing_vlaues_meadian(test_data)
+
+# create dir to store the processed
+data_path = os.path.join("data", "processed")
+os.makedirs(data_path)
+
+# Convert data to CSV and create csv file
+train_processed_data.to_csv(os.path.join(data_path,"train_processed.csv"),index=False)
+test_processed_data.to_csv(os.path.join(data_path,"test_processed.csv"),index=False)
